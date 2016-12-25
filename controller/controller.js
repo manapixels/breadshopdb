6d4d9ae708dc5ea2dff08c40d54a98d046603ab4
Zhenyang <zhenyang_lim@mymail.sutd.edu.sg>
2016-12-25 17:58:00 +0800

    committttt

---
+++ controller/controller.js
@@ -0,0 +1,21 @@
+function AppCtrl() {
+    console.log("hello world from controller");
+    
+    bread1 = {
+        id: "000001",
+        name: "Velvety Choco Bread",
+        description: "Melting chocolate lava bread within!",
+        price: "100"
+    });
+    
+    bread2 = {
+        id: "000002",
+        name: "Velvety Red Bean Bread",
+        description: "Melting chocolate lava bread within!",
+        price: "100"
+    });
+    
+    
+    
+    
+};
